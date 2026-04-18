8d9acff946f231104b42b8f0025fa48b72f7e55a
sauron918 <sauron918@gmail.com>
2026-04-18 14:00:36 +0300

    feat(espanso): add espanso configs

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -18,6 +18,8 @@ brew install \
   espanso
 
 # stop – for configs sync
-# espanso – for text replacements
+# espanso – for text replacements, it needs to be run before stow usage to avoid stow errors 'unable to load config: unable to load config'
+
+echo "2. Configuring..."
 
 echo "Done!"
